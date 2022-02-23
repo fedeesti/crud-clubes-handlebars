caf17093e80d35dddb55ba74253d69c308f3bad1
fedeesti <fede.estigarribia@hotmail.com>
2022-02-23 16:02:45 -0300

    Create new teams

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const fs = require('fs');
+const multer = require('multer');
+const teams = require('../data/equipos.db.json');
+const { createTeam } = require('./services/index');
 
 const URL = 'http://localhost:';
 const PORT = 8080;
 
 const app = express();
 const hbs = exphbs.create();
+const upload = multer({ dest: './uploads/imagenes' });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -23,6 +28,24 @@ app.get('/', function (req, res) {
       teams,
     },
   });
+});
+
+app.get('/create-team', function (req, res) {
+  res.render('create-team', {
+    layout: 'main',
+  });
+});
+
+app.post('/create-team', upload.single('crestUrl'), function (req, res) {
+  const newTeam = createTeam(req.body, req.file.filename);
+  teams.push(newTeam);
+  const newListTeams = JSON.stringify(teams, null, 2);
+  fs.writeFile('./data/equipos.db.json', newListTeams, function (err) {
+    if (err) throw new Error("Can't create team");
+    console.log('Team added');
+  });
+
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
---
+++ src/services/index.js
@@ -0,0 +1,51 @@
+const teams = require('../../data/equipos.db.json');
+const competitions = require('../../data/competitions.json');
+
+function createTeam(teamData, logo) {
+  const newTeam = {
+    id: generateId(),
+    area: {
+      id: findCountryId(teamData.country),
+      name: teamData.country,
+    },
+    name: teamData.name,
+    shortName: teamData.shortName,
+    tla: teamData.name.substring(0, 3).toUpperCase(),
+    crestUrl: logo === '' ? null : `/imagenes/${logo}`,
+    address: teamData.address === '' ? null : teamData.address,
+    phone: teamData.phone === '' ? null : teamData.phone,
+    website: teamData.website === '' ? null : teamData.website,
+    email: teamData.email === '' ? null : teamData.email,
+    founded: teamData.founded === '' ? null : parseInt(teamData.founded),
+    clubColors: teamData.clubColors.replaceAll(' ', ' / '),
+    venue: teamData.venue === '' ? null : teamData.venue,
+    lastUpdated: new Date(),
+  };
+
+  return newTeam;
+}
+
+function generateId() {
+  const allId = [];
+  const uuid = Math.trunc(Math.floor(Math.random() * Date.now()) / 1000000000);
+
+  teams.forEach((team) => {
+    allId.push(team.id);
+  });
+
+  if (allId.includes(uuid)) {
+    generateId();
+  }
+
+  return uuid;
+}
+
+function findCountryId(country) {
+  for (let i = 0; i <= competitions.length; i++) {
+    if (competitions[i].country === country) {
+      return competitions[i].id;
+    }
+  }
+}
+
+module.exports = { createTeam };
