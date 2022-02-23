70175a47668042936195bb19e272eafcf5b6e47f
fedeesti <fede.estigarribia@hotmail.com>
2022-02-23 16:47:47 -0300

    Add edit team
    The edit team page is added including the logo.

--- src/services/index.js
+++ src/services/index.js
@@ -25,6 +25,30 @@ function createTeam(teamData, logo) {
   return newTeam;
 }
 
+function updateTeam(teamData, logo, id) {
+  const team = {
+    id: id,
+    area: {
+      id: findCountryId(teamData.country),
+      name: teamData.country,
+    },
+    name: teamData.name,
+    shortName: teamData.shortName,
+    tla: teamData.name.substring(0, 3).toUpperCase(),
+    crestUrl: logo,
+    address: teamData.address === '' ? null : teamData.address,
+    phone: teamData.phone === '' ? null : teamData.phone,
+    website: teamData.website === '' ? null : teamData.website,
+    email: teamData.email === '' ? null : teamData.email,
+    founded: teamData.founded === '' ? null : parseInt(teamData.founded),
+    clubColors: teamData.clubColors,
+    venue: teamData.venue === '' ? null : teamData.venue,
+    lastUpdated: new Date(),
+  };
+
+  return team;
+}
+
 function generateId() {
   const allId = [];
   const uuid = Math.trunc(Math.floor(Math.random() * Date.now()) / 1000000000);
@@ -48,4 +72,4 @@ function findCountryId(country) {
   }
 }
 
-module.exports = { createTeam };
+module.exports = { createTeam, updateTeam };
